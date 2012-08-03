12109d4671c69c67f17f552b3568264bed00478a
Martin Smith <martin.audacis@gmail.com>
2012-08-03 16:07:53 -0700

    Now with Golden Array construction

--- multest.cpp
+++ multest.cpp
@@ -1,22 +1,34 @@
 #include <iostream>
 #include "naiveFunctions.h"
 #include "muller.h"
+#include <cstdlib>
 using namespace std;
+
+#define DIM1 20
+#define DIM2 20
+float A[DIM1*DIM2];
+float B[DIM2*DIM1];
+
 
 int main()
 {
+    srand((unsigned)time(0));
+
+    for (int i = 0; i < DIM1*DIM2; i++)
+    {
+        A[i] = (rand()%100)+1;
+        B[i] = (rand()%100)+1;
+    }
+
+    float* goldenC = naive_matrix_multiply(A,B, DIM1, DIM2, DIM1);
+
     NaiveMuller nM = NaiveMuller();
-    nM.test();
     Muller* m = &nM;
     m->test();
     cout << "main checking in" << endl;
     return 0;
 }
 
-// XXX make the test arrays (A, B)
-
-
-// XXX make the muller object
 // XXX pass in the arrays
 // XXX get back the arrays
 // XXX compare to the output of naiveFunctions.h
